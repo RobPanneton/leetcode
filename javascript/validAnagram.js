f9df0b190950d3910871e03632f3e7baa41b7923
RobPanneton <robert.panneton@gmail.com>
2023-12-05 14:21:14 -0500

    add solution using a map

--- javascript/validAnagram.js
+++ javascript/validAnagram.js
@@ -1,4 +1,4 @@
-const { performance } = require("node:perf_hooks");
+const { performance } = require("perf_hooks");
 
 const isAnagramSplit = (s, t) =>
   s.split("").sort().join() === t.split("").sort().join();
@@ -26,6 +26,22 @@ const isAnagramArray = (s, t) => {
       return false;
     }
   }
+
+  return true;
+};
+
+const isAnagramMap = (s, t, map = new Map()) => {
+  if (s.length !== t.length) return false;
+
+  for (const c of s) map.set(c, (map[c] || 0) + 1);
+
+  for (const c of t) {
+    if (!map.has(c)) return false;
+    if (map[c] <= 0) return false;
+    map.set(c, map[c] - 1);
+  }
+
+  for (const [char, count] of map) if (count === 0) return false;
 
   return true;
 };
@@ -69,6 +85,15 @@ runTest(
   "thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting"
 );
 
+runTest(isAnagramMap, "anagram", "nagaram");
+runTest(isAnagramMap, "not", "one");
+runTest(isAnagramMap, "a", "ab");
+runTest(
+  isAnagramMap,
+  "thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting",
+  "thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting"
+);
+
 // RESULTS :
 
 // split
@@ -100,6 +125,16 @@ runTest(
 // Duration:  0.002299997955560684
 // Running test:  isAnagramArray . With arguments: thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting, thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting
 // Duration:  0.021699998527765274
+
+//map
+// Running test:  isAnagramMap . With arguments: anagram, nagaram
+// Duration:  0.19929999858140945
+// Running test:  isAnagramMap . With arguments: not, one
+// Duration:  0.031199999153614044
+// Running test:  isAnagramMap . With arguments: a, ab
+// Duration:  0.003400001674890518
+// Running test:  isAnagramMap . With arguments: thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting, thisisamuchlongerstringthatwewillusefortestestingthisisamuchlongerstringthatwewillusefortestesting
+// Duration:  0.18459999933838844
 
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
